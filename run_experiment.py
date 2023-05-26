f459241086bf5088c01200453475196888d7177a
maizi <1319634411@qq.com>
2023-05-26 12:17:46 +0800

    commit

--- run_experiment.py
+++ run_experiment.py
@@ -6,11 +6,11 @@ from omegaconf import DictConfig
 from pytorch_lightning import Trainer
 from pytorch_lightning.callbacks import Callback, EarlyStopping, ModelCheckpoint
 
-from ..handwritten_multi_digit_number_recognition.data import MultiDigitMNIST
-from ..handwritten_multi_digit_number_recognition.lit_models import CTCLitModel
+from handwritten_multi_digit_number_recognition.data import MultiDigitMNIST
+from handwritten_multi_digit_number_recognition.lit_models import CTCLitModel
 
 
-@hydra.main(config_path="../", config_name="config")
+@hydra.main(config_path="./", config_name="config")
 def main(cfg: DictConfig):
     datamodule = MultiDigitMNIST(**cfg.data)
     datamodule.prepare_data()
