28d90913dd924d46bee153db49935feb425ac706
maizi <1319634411@qq.com>
2023-05-26 18:45:26 +0800

    commit

--- rec.py
+++ rec.py
@@ -1,10 +1,17 @@
-import logging
-
+import argparse
 import handwritten_multi_digit_number_recognition.utils as utils
 from handwritten_multi_digit_number_recognition import Recognizer
 
+# 创建命令行参数解析器
+parser = argparse.ArgumentParser(description="Predict handwritten multi-digit number")
+parser.add_argument('image_path', type=str, help="Path to the image file")
+
+# 解析命令行参数
+args = parser.parse_args()
+
 model = Recognizer()
 
-pred_num = model.predict("/kaggle/working/src/images/test_image.png")
+# 使用命令行参数中的图片地址
+pred_num = model.predict(args.image_path)
 
 print(f"pred_num:{pred_num}")
